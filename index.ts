0393c69792052de26f08ba8d6fa9b53c549ef9c1
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2025-02-04 20:30:41 +0500

    operations

--- index.ts
+++ index.ts
@@ -1,63 +1,120 @@
-import inquirer from "inquirer";
-import chalk from "chalk";
+// // import inquirer from "inquirer";
+// // import chalk from "chalk";
 
-const askUser = await inquirer.prompt({
-  name: "age",
-  type: "number",
-  message: "Enter Your Age",
-});
+// // const askUser = await inquirer.prompt({
+// //   name: "age",
+// //   type: "number",
+// //   message: "Enter Your Age",
+// // });
 
-console.log(
-  chalk.bold(`InshaAllah in ${60 - askUser.age} years you will be 60 years old`)
-);
-console.log("Hello world");
-// download node
+// // console.log(
+// //   chalk.bold(`InshaAllah in ${60 - askUser.age} years you will be 60 years old`)
+// // );
+// console.log("Hello world");
+// // download node
 
-// const str: string = "string";
+// // const str: string = "string";
 
-// let abc;
+// // let abc;
 
-// abc = "";
+// // abc = "";
 
-// abc = 34;
+// // abc = 34;
 
-// Type Inference
-// let country = 'Pakistan';
+// // Type Inference
+// // let country = 'Pakistan';
 
-// country = 90;
+// // country = 90;
 
-// type is implicitly is setting "any".
-// function Name(params) {
+// // type is implicitly is setting "any".
+// // function Name(params) {
 
+// // }
+
+// // Name('')
+// // npm init
+// // npm init -y
+// // npm i @type/node -D
+
+// // TypeScript Part 1
+// // https://docs.google.com/presentation/d/1eepDn27eQ8DbRJy7LHTkrIwYx8hQwuo0aDKGS_x10yc/edit#slide=id.g262ab56d35d_1_155
+// // TypeScript Part 2
+// // https://docs.google.com/presentation/d/1mFtLVoMnXnSVtIRSr3GPCgIqgt-g_hgEqmV4qDBUxwg/edit#slide=id.gd1bf8d60a4_0_0
+
+// // function Name(params:'type'): string {
+// //     // return 2+2
+// // }
+// // Name('type')
+
+// // let _username: "mufzan94mld";
+
+// // _username = "mufzan94mld";
+
+// // let country = "Pakistan";
+
+// // country = ''
+
+// // let variable;
+
+// let typeUnknown: unknown = "";
+
+// typeUnknown = 897;
+
+// console.log(typeUnknown as number);
+
+// let card: any = NaN;
+// card = "";
+// card = 23;
+
+// let cards: unknown = "";
+// cards = () => {
+//   return 3;
+// };
+
+// let card1: number = 32;
+// card = 32;
+// // card1 = "";
+// let Bool = false;
+
+// // Bool = () => {
+// //   return 2;
+// // };
+
+// // 0,
+// // 1
+
+// if (card) {
 // }
+// Boolean("");
 
-// Name('')
-// npm init
-// npm init -y
-// npm i @type/node -D
+// let variable = "message";
+// // console.mufzan;
+
+// // types = "";
+
+// const name = "Muhammad" + " " + "Zeeshan" + " " + "Khan";
+
+// const surName = "Muhammad";
+// const firstName = "Zeeshan";
+// const lastName = "Khan";
+
+// const fullName = `${surName} ${firstName} ${lastName}`;
+// const concate = surName + " " + firstName + " " + lastName;
+
+// alert("Error");
+
+const num = 21;
+const str = "noask";
+const boolVal = true;
+
+// console.log(num + 3 + str);
+// console.log(num + boolVal);
+
+// function addBool(a: number, b: boolean): unknown {
+//   return a * b;
+// }
 
 // TypeScript Part 1
 // https://docs.google.com/presentation/d/1eepDn27eQ8DbRJy7LHTkrIwYx8hQwuo0aDKGS_x10yc/edit#slide=id.g262ab56d35d_1_155
 // TypeScript Part 2
 // https://docs.google.com/presentation/d/1mFtLVoMnXnSVtIRSr3GPCgIqgt-g_hgEqmV4qDBUxwg/edit#slide=id.gd1bf8d60a4_0_0
-
-// function Name(params:'type'): string {
-//     // return 2+2
-// }
-// Name('type')
-
-// let _username: "mufzan94mld";
-
-// _username = "mufzan94mld";
-
-// let country = "Pakistan";
-
-// country = ''
-
-// let variable;
-
-let typeUnknown: unknown = "";
-
-typeUnknown = 897;
-
-console.log(typeUnknown as number);
